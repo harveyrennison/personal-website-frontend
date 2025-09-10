09b96f625c5eb642e188b599d28d39a786bbd76d
harveyrennison <hre67@uclive.ac.nz>
2025-09-10 21:40:34 +1200

    added email icon for ability for people to contact me via email

--- my-app/src/layout/footer.tsx
+++ my-app/src/layout/footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Typography, Stack, IconButton, Link } from "@mui/material";
-import { GitHub, LinkedIn } from "@mui/icons-material";
+import { GitHub, LinkedIn, Email } from "@mui/icons-material";
 
 const Footer = () => {
   return (
@@ -30,33 +30,50 @@ const Footer = () => {
         </Typography>
         
         <Stack direction="row" spacing={1}>
-          <IconButton 
-            component={Link} 
-            href="https://github.com/harveyrennison" 
-            target="_blank"
-            size="small"
-            sx={{ 
-              color: 'grey.400', 
-              '&:hover': {  // want to update the hover so it looks like the app itself
-                color: 'white'
-              } 
-            }}
-          >
-            <GitHub fontSize="small" />
-          </IconButton>
-          <IconButton 
-            component={Link} 
-            href="https://linkedin.com/in/harveyrennison" 
-            target="_blank"
-            size="small"
-            sx={{ 
-              '&:hover': { 
-                backgroundColor: 'action.hover'
-              } 
-            }}
-          >
-            <LinkedIn fontSize="small" />
-          </IconButton>
+
+            <IconButton 
+                component={Link} 
+                href="https://github.com/harveyrennison" 
+                target="_blank"
+                size="small"
+                sx={{ 
+                color: 'grey.400', 
+                '&:hover': {  // want to update the hover so it looks like the app itself
+                    color: 'white'
+                } 
+                }}
+            >
+                <GitHub fontSize="small" />
+            </IconButton>
+
+            <IconButton 
+                component={Link} 
+                href="https://linkedin.com/in/harveyrennison" 
+                target="_blank"
+                size="small"
+                sx={{ 
+                '&:hover': { 
+                    backgroundColor: 'action.hover'
+                } 
+                }}
+            >
+                <LinkedIn fontSize="small" />
+            </IconButton>
+
+            <IconButton 
+                component={Link} 
+                href="mailto:harveyrennison@gmail.com" 
+                target="_blank"
+                size="small"
+                sx={{ 
+                    '&:hover': { 
+                    backgroundColor: 'action.hover'
+                    } 
+                }}
+            >
+                <Email fontSize="small" />
+            </IconButton>
+            
         </Stack>
       </Stack>
     </Box>
