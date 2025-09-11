9f244b961a0196c1f9164de91176177c57fe09be
harveyrennison <hre67@uclive.ac.nz>
2025-09-11 17:42:08 +1200

    added structure to my home page. i need to start working on my backend to store images, info, and more.

--- my-app/src/components/home.tsx
+++ my-app/src/components/home.tsx
@@ -3,34 +3,34 @@ import { Typography, Box } from '@mui/material';
 
 const Home = () => {
   return (
-    <Box
-        sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center"
-      }}>
+    <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", gap: 3}}> {/* Seperates greeting and features projects */}
+        
+        {/* Contains the greeting and image */}
+        <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
 
-        {/* Left side */}
-        <Box sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            gap: 3, // vertical space between inner boxes
-            }}
-        >
-            <Box>
-                <Typography variant="h2">Hello!</Typography>
-                <Typography variant="h3">I'm Harvey Rennison</Typography>
+            {/* Left side */}
+            <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center", gap: 3 }}>
+                <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center", gap: 2 }}>
+                    <Typography variant="h2" fontWeight={700}>Hello!</Typography>
+                    <Typography variant="h2" fontWeight={700}>I'm Harvey Rennison</Typography>
+                </Box>
+                <Box>
+                    <Typography variant="h6">A passionate full-stack developer from Christchurch, New Zealand</Typography>
+                </Box>
             </Box>
+
+            {/* Right side */}
             <Box>
-                <Typography variant="h6">A passionate frontend developer from Christchurch, New Zealand</Typography>
+                <Typography variant="h3">insertImageHere</Typography>
             </Box>
         </Box>
-        {/* Right side */}
-        <Box>
-            <Typography variant="h3">insertImageHere</Typography>
+        <Box sx={{ display: "flex", justifyContent: "column", alignItems: "center" }}>
+            <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center", gap: 2 }}>
+                <Typography variant="h3">Featured Projects!</Typography>
+                <Typography variant="h3">insertProjects lorem lorem lorem lorem lorem lorem lorem</Typography> 
+            </Box>
         </Box>
-    </Box>
+    </Box> // need to store project info in backend, then display featured projects here
   );
 };
 
